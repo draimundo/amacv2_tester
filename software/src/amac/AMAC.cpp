b742efc9d3bd0f203bee0879c5144892032adde5
Daniel Raimundo <draimundo@lbl.gov>
2018-07-14 23:01:39 +0000

    Register initialisations + hard/software connection in AMAC class

--- software/src/amac/AMAC.cpp
+++ software/src/amac/AMAC.cpp
@@ -1,7 +1,48 @@
 #include "AMAC.h"
 
-AMAC::AMAC()
-{ }
+// AMAC::AMAC() : AMACv2Reg(){
+	
+// }
 
-AMAC::~AMAC()
-{ }
+AMAC::AMAC(unsigned short amacid, std::shared_ptr<DeviceCom> fpgaCom) : AMACv2Reg(), EndeavourCom(amacid, fpgaCom){
+	for (auto const& p : regMap){
+		if((this->*p.second).canBeWritten()){
+			syncReg(p.second);
+		}
+	}
+}
+
+AMAC::~AMAC(){}
+
+void AMAC::wrEndeavourReg(uint32_t address, uint32_t data){
+	this->write_reg(address, data);
+}
+uint32_t AMAC::rdEndeavourReg(uint32_t address){
+	return this->read_reg(address);
+}
+
+void AMAC::wrVirtualField(AMACv2Field AMACv2Reg::* ref, uint32_t data){
+	setField(ref, data);
+}
+void AMAC::wrVirtualReg(AMACv2Field AMACv2Reg::* ref, uint32_t data){
+	(this->*ref).writeRaw(data);
+}
+uint32_t AMAC::rdVirtualField(AMACv2Field AMACv2Reg::* ref){
+	return getField(ref);
+}
+uint32_t AMAC::rdVirtualReg(AMACv2Field AMACv2Reg::* ref){
+	return (this->*ref).readRaw();
+}
+
+void AMAC::syncReg(AMACv2Field AMACv2Reg::* ref){
+	wrEndeavourReg(getAddr(ref), (this->*ref).readRaw());
+}
+void AMAC::wrField(AMACv2Field AMACv2Reg::* ref, uint32_t data){
+	wrVirtualField(ref, data);
+	wrEndeavourReg(getAddr(ref), (this->*ref).readRaw());
+}
+uint32_t AMAC::rdField(AMACv2Field AMACv2Reg::* ref){
+	uint32_t ret = rdEndeavourReg(getAddr(ref));
+	wrVirtualReg(ref, ret);
+	return rdVirtualField(ref);
+}
