ac19fe0c29539848484ea4efdad5d55925f2934b
Joseph Reichert <joseph.reichert@cern.ch>
2018-07-26 22:44:24 +0000

    cleanup of adc tester

--- software/src/tools/ltc2333test.cpp
+++ software/src/tools/ltc2333test.cpp
@@ -21,43 +21,32 @@ int main(int argc, char* argv[])
   tb.powerOn();
   tb.powerAMACOn();
   std::vector<std::pair<uint8_t, uint8_t>> vec;
-  //vec.push_back(std::make_pair(0,0));
-  //vec.push_back(std::make_pair(1,0));
   vec.push_back(std::make_pair(2,0));
-  //vec.push_back(std::make_pair(3,0));
-  //vec.push_back(std::make_pair(4,0));
-  //vec.push_back(std::make_pair(5,0));
-  //vec.push_back(std::make_pair(6,0));
-  //vec.push_back(std::make_pair(7,0));
   tb.ADC0.setADC(vec);
   usleep(3E6);
   tb.ADC0.setADC(vec);
   try
-    {
-      // Create the communication object
-			std::cout << "before while" << std::endl;
+  {
+	  // Create the communication object
+	  std::cout << "before while" << std::endl;
+	  while(true){
+		  //tb.ADC0.setADC(vec);
+		  std::vector<LTC2333Outputs> outputs = tb.ADC0.getADC();
+		  usleep(1E6);
+		  for(int i = 0; i < outputs.size(); ++i){
+			  auto output = outputs.at(i);
+			  std::cout << i << ": " << output.result * 5. / 65535. << " aka " << output.result << std::endl;
+		  }
+	  }
 
-
-      while(true){
-				//tb.ADC0.setADC(vec);
-        std::vector<LTC2333Outputs> outputs = tb.ADC0.getADC();
-
-				usleep(1E6);
-				
-        for(int i = 0; i < outputs.size(); ++i){
-          auto output = outputs.at(i);
-					std::cout << i << ": " << output.result * 5. / 65535. << " aka " << output.result << std::endl;
-        }
-      }
-
-    }
+  }
   catch(ComIOException e){
-			std::cout << "caught" << std::endl;
-			std::cout << "errno = " << errno << std::endl;
-      std::cout << e.what() << std::endl;
-    }
-	catch(...){
-		std::cout << "caught something" << std::endl;
-	}
+	  std::cout << "caught" << std::endl;
+	  std::cout << "errno = " << errno << std::endl;
+	  std::cout << e.what() << std::endl;
+  }
+  catch(...){
+	  std::cout << "caught something" << std::endl;
+  }
   return 0;
 }
