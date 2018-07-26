afcfd28c0d27df98a31ee6510b28d757e87e19e1
Karol Krizka <kkrizka@gmail.com>
2018-07-26 15:46:34 +0000

    EndeavourBER uses BER test from AMACTest.

--- software/src/tests/EndeavourBER.cpp
+++ software/src/tests/EndeavourBER.cpp
@@ -1,67 +1,42 @@
 #include "AMACTB.h"
-
-#include "EndeavourComException.h"
+#include "AMAC.h"
+#include "AMACTest.h"
 
 #include <unistd.h>
 #include <iostream>
 #include <iomanip>
 #include <vector>
+#include <memory>
 
 int main()
 {
-  AMACTB TB;
+  std::shared_ptr<AMACTB> TB=std::make_shared<AMACTB>();
+  AMACTest test("AMACREF1",TB);
 
   // Power on
-  TB.powerOn();
+  TB->powerOn();
   usleep(1E6); // 1s wait for a clean power up
   std::cout << "Power ON" << std::endl;
 
   // Configure I/O pads
-  TB.setIDPads(0x1F);
-  TB.setIO(TB.ResetB, true);
+  TB->setIDPads(0x1F);
+  TB->setIO(TB->ResetB, true);
 
   // Power on AMAC
-  TB.powerAMACOn();
+  TB->powerAMACOn();
   std::cout << "AMAC ON" << std::endl;
   usleep(1E6);
 
-  // Set ID
-  TB.END.setid(EndeavourCom::REFMODE::IDPads, 0x1F);
-  std::cout << "SETID" << std::endl;
-  usleep(1E6);
+  // Initialize the AMAC communication
+  TB->END.init();
+  std::cout << "Endeavour OK" << std::endl;
+  usleep(1e6);
 
   // BER loop
-  uint good=0;
-  uint total=100000;
-  for(uint i=0; i<total; i++)
-    {
-      try
-	{      
-	  uint valin=rand()*0xFFFFFFFF;
-	  TB.END.write_reg(166,valin);
-	  usleep(1e3);
-	  uint valout=TB.END.read_reg(166);
-	  usleep(1e3);
-	  if(valin==valout)
-	    {
-	      good++;
-	    }
-	  else
-	    {
-	      std::cout << "Write: 0x" << std::hex << valin << ", Read: " << valout << std::dec << std::endl;
-	    }
-	}
-      catch(EndeavourComException e)
-	{
-	  std::cout << "EndeavourComException" << std::endl;
-	  std::cout << e.what() << std::endl;
-	}
-    }
-
-  std::cout << "Reliability: " << ((float)good)/total << std::endl;
+  test.runBERvsClock();
 
   // Power off
-  TB.powerOff();
+  TB->powerOff();
   std::cout << "Power OFF" << std::endl;
   usleep(1E6); // 1s wait for a clean power down
 
