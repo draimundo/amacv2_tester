6fb692df736e48b84fe538e18b59af1dcb556781
Joseph Reichert <joseph.reichert@cern.ch>
2018-07-26 22:36:12 +0000

    removing old code

--- software/src/devcom/ADCDevice.h
+++ software/src/devcom/ADCDevice.h
@@ -12,12 +12,6 @@ public:
   virtual void init() = 0;
   virtual void reset() = 0;
 
-  // Sets next conversion result for specified channel and span, and
-  // reads out previous conversion result.
-  //virtual std::vector<unsigned int> setNextConversion(unsigned int chan, unsigned int span) = 0;
-
-  // Sends dummy signal and reads out previous conversion result
-  //virtual std::vector<unsigned int> readPreviousConversion() = 0;
 };
 
 #endif // ADCDevice_H
