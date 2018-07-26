ada18fd1de31254fd22ac02d3d2f36cc59f41831
Karol Krizka <kkrizka@gmail.com>
2018-07-26 11:44:15 -0400

    Add AMACDumpRegisters test. Implement BER and RegDump tests in AMACTest.

--- software/src/amac/AMACTest.cpp
+++ software/src/amac/AMACTest.cpp
@@ -1,7 +1,62 @@
 #include "AMACTest.h"
 
-AMACTest::AMACTest()
+#include <iostream>
+#include <iomanip>
+
+#include <unistd.h>
+
+#include "EndeavourComException.h"
+
+AMACTest::AMACTest(const std::string& name, std::shared_ptr<AMACTB> amactb)
+  : m_name(name), m_amactb(amactb)
 { }
 
 AMACTest::~AMACTest()
 { }
+
+void AMACTest::dumpRegisters()
+{
+  uint data;
+  for(uint reg=0;reg<172;reg++)
+    {
+      data=(reg==0)?m_amactb->END.read_reg(reg):m_amactb->END.readnext_reg();
+      std::cout << reg << "\t0x" << std::hex << std::setw(8) << std::setfill('0') << data << std::dec << std::endl;
+    }
+}
+
+void AMACTest::runBERvsClock()
+{
+  runBER();
+}
+
+float AMACTest::runBER()
+{
+  uint good=0;
+  uint total=10000;
+  for(uint i=0; i<total; i++)
+    {
+      try
+	{      
+	  uint valin=rand()*0xFFFFFFFF;
+	  m_amactb->END.write_reg(166,valin);
+	  usleep(1e3);
+	  uint valout=m_amactb->END.read_reg(166);
+	  usleep(1e3);
+	  if(valin==valout)
+	    {
+	      good++;
+	    }
+	  else
+	    {
+	      std::cout << "Write: 0x" << std::hex << valin << ", Read: " << valout << std::dec << std::endl;
+	    }
+	}
+      catch(EndeavourComException e)
+	{
+	  std::cout << "EndeavourComException" << std::endl;
+	  std::cout << e.what() << std::endl;
+	}
+    }
+
+  std::cout << "Reliability: " << ((float)good)/total << std::endl;
+}
--- software/src/amac/AMACTest.h
+++ software/src/amac/AMACTest.h
@@ -1,11 +1,26 @@
 #ifndef AMACTEST_H_
 #define AMACTEST_H_
 
+#include <memory>
+
+#include "AMACTB.h"
+
 class AMACTest 
 {
 public:
+  AMACTest(const std::string& name, std::shared_ptr<AMACTB> amactb);
   AMACTest();
   ~AMACTest();
+
+  void dumpRegisters();
+  
+  float runBER();  
+  void runBERvsClock();
+
+private:
+  std::string m_name;
+  
+  std::shared_ptr<AMACTB> m_amactb;
 };
 
 #endif // AMACTEST_H_
